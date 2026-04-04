6dae44780038ed61aa458a26a83a0335769cb34e
GermanViter <german.viter@icloud.com>
2026-04-04 11:32:24 -0400

    commencer la boucle principale du jeux

--- src/com/rpg/main/Main.java
+++ src/com/rpg/main/Main.java
@@ -3,23 +3,35 @@ package com.rpg.main;
 import com.rpg.entities.Mage;
 import com.rpg.entities.Warrior;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
 
         String playerName = "";
-        displayMenu(sc, playerName);
-
+        playerName = displayMenu(sc, playerName);
         Mage mage = new Mage(playerName);
         Warrior war = new Warrior(playerName);
         String playerChoice = sc.nextLine();
 
+        if (playerChoice.toUpperCase().equals("Q")) {
+            return;
+        }
+
         var pl = (playerChoice.equals("1")) ? mage : (playerChoice.equals("2")) ? war : null;
-        System.out.println(pl);
+        while (true) {
+            if (pl == null) {
+                System.out.print("Choisisses 1 ou 2 :");
+                pl = (playerChoice.equals("1")) ? mage : (playerChoice.equals("2")) ? war : null;
+                continue;
+            }
+
+        }
     }
 
-    public static void displayMenu(Scanner sc, String pl) {
+    public static String displayMenu(Scanner sc, String pl) {
 
         System.out.println("<======================>");
         System.out.println("Bien venu dans la Shadow Arena");
@@ -30,5 +42,6 @@ public class Main {
         System.out.println("[2] Warior ~~ HP : 150, ATQ : 20, SPE : attaque lourde");
         System.out.println("[Q] Quitter");
         System.out.println("<======================>");
+        return pl;
     }
 }
