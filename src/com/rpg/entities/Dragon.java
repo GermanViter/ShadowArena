59562d6c3d4e178be68d62e7e19890efec726f07
GermanViter <german.viter@icloud.com>
2026-04-06 20:03:04 -0400

    fix(monsers) : ajuster les hp des monstres pour rendre le jeu plus dur

--- src/com/rpg/entities/Dragon.java
+++ src/com/rpg/entities/Dragon.java
@@ -7,17 +7,18 @@ import com.rpg.core.ConsoleUtils;
 public class Dragon extends Monster {
 
     public Dragon() {
-        super("Dragon Ancien", 300, 300, new String[]{
-            "crache un feu ancestral sur vous!",
-            "vous frappe avec sa queue massive!",
-            "rugit, créant une onde de choc!",
-            "mord avec la force des montagnes!"
+        super("Dragon Ancien", 400, 400, new String[] {
+                "crache un feu ancestral sur vous!",
+                "vous frappe avec sa queue massive!",
+                "rugit, créant une onde de choc!",
+                "mord avec la force des montagnes!"
         });
     }
 
     @Override
     public void attack(GameCharacter target) {
-        ConsoleUtils.slowPrint(ConsoleColors.PURPLE_BOLD + getName() + ConsoleColors.RESET + " " + ConsoleColors.RED + getRandomAttackMessage() + ConsoleColors.RESET);
+        ConsoleUtils.slowPrint(ConsoleColors.PURPLE_BOLD + getName() + ConsoleColors.RESET + " " + ConsoleColors.RED
+                + getRandomAttackMessage() + ConsoleColors.RESET);
         target.takeDamage(40);
     }
 }
