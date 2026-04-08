9d997fbd57086dcf881a8dc0cc9ce0395cae89f8
GermanViter <german.viter@icloud.com>
2026-04-08 18:31:01 -0400

    feat(core) : implementer la persistance JSON pour gerer les type en utilisant le polymorphysme
    Ceci empeche le jeu de crash car, de base, Hero est dans le json file
    mais c'est une classe abstraite donc gson ne peux pas creer d'instance.
    Avec la persistance JSON, on peux specfier le type de hero de
    l'utilisateur mais aussi des objets dans l'inventaire

--- src/com/rpg/core/SaveManager.java
+++ src/com/rpg/core/SaveManager.java
@@ -1,14 +1,45 @@
 package com.rpg.core;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import java.io.*;
+import java.lang.reflect.Type;
+import com.rpg.entities.*;
 
 public class SaveManager {
     private static Gson gson;
 
     static {
+        JsonDeserializer<Hero> heroDeserializer = new JsonDeserializer<Hero>() {
+            @Override
+            public Hero deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                    throws JsonParseException {
+                JsonObject jsonObject = json.getAsJsonObject();
+                String className = jsonObject.get("className").getAsString();
+                try {
+                    return context.deserialize(jsonObject, Class.forName(className));
+                } catch (ClassNotFoundException e) {
+                    throw new JsonParseException("Could not find class: " + className, e);
+                }
+            }
+        };
+
+        JsonDeserializer<GameItem> itemDeserializer = new JsonDeserializer<GameItem>() {
+            @Override
+            public GameItem deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                    throws JsonParseException {
+                JsonObject jsonObject = json.getAsJsonObject();
+                String className = jsonObject.get("className").getAsString();
+                try {
+                    return context.deserialize(jsonObject, Class.forName(className));
+                } catch (ClassNotFoundException e) {
+                    throw new JsonParseException("Could not find class: " + className, e);
+                }
+            }
+        };
+
         gson = new GsonBuilder()
+                .registerTypeAdapter(Hero.class, heroDeserializer)
+                .registerTypeAdapter(GameItem.class, itemDeserializer)
                 .setPrettyPrinting()
                 .create();
 
@@ -25,7 +56,27 @@ public class SaveManager {
 
     public static void savePlayer(PlayerSave data, String username) {
         try (FileWriter writer = new FileWriter("users_info/" + username + ".json")) {
-            gson.toJson(data, writer);
+            JsonElement element = gson.toJsonTree(data);
+            if (element.isJsonObject()) {
+                JsonObject root = element.getAsJsonObject();
+
+                // Add className to Hero
+                JsonObject heroObj = root.getAsJsonObject("hero");
+                if (heroObj != null) {
+                    heroObj.addProperty("className", data.getHero().getClass().getName());
+                }
+
+                // Add className to each item in inventory
+                JsonArray inventoryArray = root.getAsJsonArray("inventory");
+                if (inventoryArray != null) {
+                    java.util.ArrayList<GameItem> items = data.getInventory();
+                    for (int i = 0; i < items.size(); i++) {
+                        JsonObject itemObj = inventoryArray.get(i).getAsJsonObject();
+                        itemObj.addProperty("className", items.get(i).getClass().getName());
+                    }
+                }
+            }
+            gson.toJson(element, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
