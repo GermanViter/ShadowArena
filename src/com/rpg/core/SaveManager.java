67afca7a2d5a6238c64351d36704c0aacb4cfb43
GermanViter <german.viter@icloud.com>
2026-04-08 17:16:27 -0400

    feat(core) : ecrit une classe qui prend les infos du player courant de la classe playersave et la transforme en json
    outils utilises : filewriter, filereader et le module gson
    
    Ceci est important pour le systeme de sauvegarde qui va probablement
    evoluer par apres

---
+++ src/com/rpg/core/SaveManager.java
@@ -0,0 +1,37 @@
+package com.rpg.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.*;
+
+public class SaveManager {
+    private static Gson gson;
+
+    static {
+        gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    public static boolean playerExists(String username) {
+        File saveFile = new File("users_info/" + username + ".json");
+        return saveFile.exists();
+    }
+
+    public static void savePlayer(PlayerSave data, String username) {
+        try (FileWriter writer = new FileWriter("users_info/" + username + ".json")) {
+            gson.toJson(data, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PlayerSave loadPlayer(String username) {
+        try (FileReader reader = new FileReader("users_info/" + username + ".json")) {
+            return gson.fromJson(reader, PlayerSave.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
