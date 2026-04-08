ba9e8902d3cdb774f8e3b6398b034319a78cd597
GermanViter <german.viter@icloud.com>
2026-04-08 14:22:50 -0400

    feat(GameCharacter and Hero) : commencer le level up feature pour plus de diversitée

--- src/com/rpg/entities/Hero.java
+++ src/com/rpg/entities/Hero.java
@@ -5,6 +5,8 @@ import com.rpg.core.GameCharacter;
 public abstract class Hero extends GameCharacter {
 
     private int damage;
+    private int level = 1;
+    private int experience = 0;
 
     public Hero(String name, int hp, int maxHp, int mana, int maxMana, int damage) {
         super(name, hp, maxHp, mana, maxMana);
@@ -17,6 +19,27 @@ public abstract class Hero extends GameCharacter {
 
     public void setDamage(int damage) {
         this.damage = damage;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void levelUp() {
+        level++;
+        damage += 5;
+        setMaxHp(getMaxHP() + 20);
+        setCurrentHP(getMaxHP());
+        setMaxMana(getMaxMana() + 10);
+        setMana(getMaxMana());
+    }
+
+    public void setHp(int hp) {
+        setCurrentHP(hp);
+    }
+
+    public int getExperience() {
+        return experience;
     }
 
 }
